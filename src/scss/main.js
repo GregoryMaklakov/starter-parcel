d6433879b4663e9314bfb52ce36f92eca092caac
Gregory <maklakov.grigory@gmail.com>
2022-12-12 10:58:37 +0100

    add new mixon & main.js with burger script

---
+++ src/scss/main.js
@@ -0,0 +1,10 @@
+"use strict";
+
+
+// Bugrer & menu
+/* let menuBtn = document.querySelector('.icon-menu');
+let menu = document.querySelector('.menu');
+menuBtn.addEventListener('click', function(){
+	menuBtn.classList.toggle('active');
+    menu.classList.toggle('active');
+}); */
